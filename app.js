6285271b3ea3164c8e91f7817fe7db9bb40cfda9
chandanaaluri <chandanaaluri05@gmail.com>
2024-10-26 21:40:09 -0500

    new endpoint

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+//var usersRouter = require('./routes/users');
+const mydataRouter = require('./routes/mydata');
 
 var app = express();
 
@@ -14,7 +15,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
-app.use(express.json());
+//app.use(express.json());
+app.use('/mydata', mydataRouter);
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
