a0e0aef08ada28ee979254f21a45577e1c2993b4
chandanaaluri <chandanaaluri05@gmail.com>
2024-10-26 22:05:51 -0500

    modified /users endpoint

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+// Initialize variables
+let value = 0;
+let total = 1;
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  // Modify values
+  value += 2;
+  total += value;
+  
+  // Send the response
+  res.send(`Total is: ${total}`);
 });
 
 module.exports = router;
