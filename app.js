ba19788027951e67bb013c431c8232384398661d
chandanaaluri <chandanaaluri05@gmail.com>
2024-10-30 14:09:46 -0500

    modified

--- app.js
+++ app.js
@@ -15,7 +15,8 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(logger('dev'));aapp.use('/mydata', mydataRouter);
+app.use(logger('dev'));
+app.use('/mydata', mydataRouter);
 app.use('/computation', computationRouter);
 
 app.use(express.urlencoded({ extended: false }));
